6df201a682f9b7137b4b64f72f4de129dff9ec36
Piroty <hiroki1423@docomo.ne.jp>
2023-02-21 13:07:30 +0900

    first commit

---
+++ src/components/TweetInput.tsx
@@ -0,0 +1,23 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+import styles from "./TweetInput.module.css";
+
+const TweetInput = () => {
+  const user = useSelector(selectUser);
+  return (
+    <div>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut();
+        }}
+      />
+    </div>
+  );
+};
+
+export default TweetInput;
